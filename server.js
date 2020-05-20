9066ac22b72eb6381f12a9db4027f42c2a0d2761
efabregatgarcia <46302261+efabregatgarcia@users.noreply.github.com>
2020-05-20 17:46:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,58 @@
-var express = require("express")
-var app = express();
-console.log("Servidor web escuchando en puerto 8000");
-app.get('/',function(req,res){res.send("Hola Mundo!");});
-app.listen(8000);
+
+const mongoose = require('mongoose');
+const MONGODB = 'mongodb+srv://EI1053:Emergentes1053@ei1053-hugdf.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(MONGODB, (err) => {     
+    useNewUrlParser: true;
+    useUnifiedTopology: true;
+    if (err) throw err;   
+    console.log('Mongoose connected!')
+})
+
+const bodyparser = require('body-parser')
+const express    = require('express')
+
+const control = require('./controllers')
+
+const app = express() //API Main object
+ 
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
+
+app.use(control.middleware) //Done
+
+app.use('/', express.static('public'))
+
+app.get('/news', control.eventStream) //Stream of Server-side events
+
+// USERS
+app.post('/user', control.createUser);
+
+app.get('/users/', control.getUsers)
+
+app.get('/user/:id', control.getUserData ) //Done
+
+// COLLECTION
+app.get('/collections', control.getCollections ) //Done
+
+app.post('/collection', control.createCollection ) //Done
+
+app.get('/collection/:id', control.searchCollection )
+
+// CARS
+app.get('/cars', control.getCars )
+
+app.post('/car', control.createCar )
+
+// COINS
+
+app.get('/coins', control.getCoins )
+
+app.post('/coin', control.createCoin)
+
+// app.get('/collection/:id', control.searchCollection ) //Done
+
+//etc ...
+
+const PORT = 8000
+app.listen(PORT)
